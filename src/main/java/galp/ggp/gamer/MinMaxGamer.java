a614931672ac6648a2fd5ec15f772879266a77ac
Unknown <laukur@gmail.com>
2018-02-14 14:14:22 +0000

    MinMaxGamer fix

--- src/main/java/galp/ggp/gamer/MinMaxGamer.java
+++ src/main/java/galp/ggp/gamer/MinMaxGamer.java
@@ -1,16 +1,9 @@
-/**
- *
- */
 package galp.ggp.gamer;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.Random;
-import java.util.logging.FileHandler;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
-import org.ggp.base.player.gamer.statemachine.sample.SampleGamer;
+import org.ggp.base.apps.player.detail.DetailPanel;
+import org.ggp.base.apps.player.detail.EmptyDetailPanel;
 import org.ggp.base.util.statemachine.Move;
 import org.ggp.base.util.statemachine.Role;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
@@ -20,96 +13,57 @@ import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 import galp.ggp.statemachine.BitSetMachineState;
 import galp.ggp.statemachine.TimeOutException;
 
-/**
- * @author frimann
- *
- */
-public class MinMaxGamer extends SampleGamer {
-	private Random randomGenerator;
-	public Logger selectMoveLogger;
-    FileHandler smfh;
+public class MinMaxGamer extends TrialSampleGamer {
 
-	public MinMaxGamer() {
-		selectMoveLogger = Logger.getLogger("selectMove");
-	    try {
-	        // This block configure the logger with handler and formatter
-	        smfh = new FileHandler("selectMove.log");
-	        logger.addHandler(smfh);
-	        SimpleFormatter formatter = new SimpleFormatter();
-	        smfh.setFormatter(formatter);
-	        // the following statement is used to log any messages
-	        logger.info("My first log";
-	    } catch (SecurityException e) {
-	        e.printStackTrace();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
+	@Override
+	public Move stateMachineSelectMove(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		/*
+		 * Random rng = new Random(); StateMachine theMachine = getStateMachine();
+		 * List<Move> moves = theMachine.getLegalMoves(getCurrentState(), getRole());
+		 *
+		 * Move bestMove = moves.get(rng.nextInt(moves.size()));
+		 */
+		Move bestMove = null;
+		System.out.println("starting the search at depth 1");
+		try {
+			for (int d = 1; d < 10; d++) {
+				System.out.println("search at depth " + d);
+				bestMove = minMaxSearch((BitSetMachineState) getCurrentState(), d,
+						timeout);
+
+			}
+		} catch (TimeOutException e) {
+			System.out.println("timeout exception");
+		}
+		System.out.println(bestMove.toString());
+
+		return bestMove;
 	}
+
 	@Override
 	public void stateMachineMetaGame(long timeout)
 			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
-		Logger logger = Logger.getLogger("MyLog");
-	    FileHandler fh;
-
-//	    try {
-//	        // This block configure the logger with handler and formatter
-//	        fh = new FileHandler("MyLogFile.log");
-//	        logger.addHandler(fh);
-//	        SimpleFormatter formatter = new SimpleFormatter();
-//	        fh.setFormatter(formatter);
-//	        // the following statement is used to log any messages
-//	        logger.info("My first log");
-//	    } catch (SecurityException e) {
-//	        e.printStackTrace();
-//	    } catch (IOException e) {
-//	        e.printStackTrace();
-//	    }
-//
-//	// ABSearch search = new ABSearch(getStateMachine(), getRole());
-//		try {
-//			for (int d = 1; d < 100; d++) {
-//				logger.info("d = " + d);
-//				miniMax((BitSetMachineState) getStateMachine().getInitialState(), d, timeout);
-//
-//			}
-//		} catch (TimeOutException e) {
-//		}
+		// ABSearch search = new ABSearch(getStateMachine(), getRole());
+		/*
+		 * Move bestMove = null; System.out.println("starting the search at depth 1");
+		 * try { for (int d = 1; d < 20; d++) { System.out.println("search at depth " +
+		 * d); bestMove = minMaxSearch((BitSetMachineState)
+		 * getStateMachine().getInitialState(), d, timeout);
+		 *
+		 * } } catch (TimeOutException e) { System.out.println("timeout exception"); }
+		 * System.out.println(bestMove.toString());
+		 */
 
 	}
 
 	@Override
-
-	public Move stateMachineSelectMove(long timeout)
-			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
-
-		Logger logger = Logger.getLogger("selectMove");
-	    FileHandler fh;
-	    try {
-	        // This block configure the logger with handler and formatter
-	        fh = new FileHandler("selectMove.log");
-	        logger.addHandler(fh);
-	        SimpleFormatter formatter = new SimpleFormatter();
-	        fh.setFormatter(formatter);
-	        // the following statement is used to log any messages
-	        logger.info("My first log, timeout = " + timeout);
-	    } catch (SecurityException e) {
-	        e.printStackTrace();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
-
-//	    logger.info("Searching");
-//    		Move selection = minMaxSearch((BitSetMachineState) getStateMachine().getInitialState(), 1, timeout);
-//	    logger.info("found move: " + selection.toString());
-//	    return selection;
-
-		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
-        int index = randomGenerator.nextInt(moves.size());
-        Move selection = moves.get(index);
-        return selection;
+	public DetailPanel getDetailPanel() {
+		return new EmptyDetailPanel();
 	}
 
-	public Move minMaxSearch(BitSetMachineState state, int d, long timeout) throws TimeOutException {
+	public Move minMaxSearch(BitSetMachineState state, int d, long timeout)
+			throws TimeOutException {
 		Move bestMove = null;
 
 		int bestValue = Integer.MIN_VALUE;
@@ -121,6 +75,7 @@ public class MinMaxGamer extends SampleGamer {
 					bestValue = value;
 					bestMove = move;
 				}
+
 
 			}
 		} catch (MoveDefinitionException e) {
@@ -143,13 +98,17 @@ public class MinMaxGamer extends SampleGamer {
 			}
 		}
 		if (d == 0)
-			return 40;
+			return 42;
 		int bestValue = Integer.MIN_VALUE;
 		int value;
 		try {
 			for (Move move : getStateMachine().getLegalMoves(state, getRole())) {
 				value = 0 - minValue(state, getRole(), move, d, timeout);
 				bestValue = Math.max(value, bestValue);
+			/*	if(bestValue > alpha) {
+					alpha = bestValue;
+					if(alpha >=beta)break;
+				}*/
 			}
 		} catch (MoveDefinitionException e) {
 			// TODO Auto-generated catch block
@@ -159,14 +118,19 @@ public class MinMaxGamer extends SampleGamer {
 		return bestValue;
 	}
 
-	private int minValue(BitSetMachineState state, Role mine, Move move, int d, long timeout) throws TimeOutException {
-		int bestValue = Integer.MAX_VALUE;
+	private int minValue(BitSetMachineState state, Role mine, Move move, int d, long timeout)
+			throws TimeOutException {
+		int bestValue = Integer.MIN_VALUE;
 		int value;
 		try {
-			for (List<Move> joinMove : getStateMachine().getLegalJointMoves(state, mine, move)) {
-				value = 0
-						- miniMax((BitSetMachineState) getStateMachine().getNextState(state, joinMove), d - 1, timeout);
+			for (List<Move> jointMove : getStateMachine().getLegalJointMoves(state, mine, move)) {
+				value = 0 - miniMax((BitSetMachineState) getStateMachine().getNextState(state, jointMove), d - 1,
+						timeout);
 				bestValue = Math.max(value, bestValue);
+				/*if(bestValue > alpha) {
+					alpha = bestValue;
+					if(alpha >=beta)break;
+				}*/
 			}
 		} catch (MoveDefinitionException | TransitionDefinitionException e) {
 			// TODO Auto-generated catch block
